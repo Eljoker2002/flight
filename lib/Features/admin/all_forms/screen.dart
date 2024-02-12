e000da2b8c7499562b729d4502353dc4fe0f6397
Eljoker2002 <se.ahmedfahmy@gmail.com>
2024-02-12 07:40:46 +0200

    ...

---
+++ lib/Features/admin/all_forms/screen.dart
@@ -0,0 +1,120 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flight/Core/app_color.dart';
+import 'package:flight/Core/dimentions/navigator.dart';
+import 'package:flight/Features/admin/every_form.dart';
+import 'package:flight/Features/admin/reverse_form(User).dart';
+import 'package:flight/Features/admin/sign_up/controller.dart';
+import 'package:flight/Features/admin/sign_up/screen.dart';
+import 'package:flight/widgets/text_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ShowAllForms extends StatefulWidget {
+  ShowAllForms({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ShowAllForms> createState() => _ShowAllFormsState();
+}
+
+class _ShowAllFormsState extends State<ShowAllForms> {
+  SignUpController signUpController = SignUpController();
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 16.w, right: 16.w),
+        child: ListView.builder(
+          itemCount: signUpController.traineeName.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.only(bottom: 15.h),
+              child: InkWell(
+                onTap: () => RouteUtils.push(
+                    context: context,
+                    screen: ShowOneFormAndPrint(
+                      traineeName: signUpController.traineeName[index],
+                    )),
+                child: Column(
+                  children: [
+                    CustomText(
+                        text: "Forms", fontSize: 30.sp, color: AppColor.blue29),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 90.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.sp),
+                        color: AppColor.blue29,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(8.sp),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                CustomText(
+                                  text: "Trainee Name: ",
+                                  fontSize: 16.sp,
+                                  color: Colors.grey,
+                                ),
+                                CustomText(
+                                  fontSize: 20.sp,
+                                  text: signUpController.traineeName[index],
+                                  color: AppColor.white,
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                CustomText(
+                                  text: "Code No.: ",
+                                  fontSize: 16.sp,
+                                  color: Colors.grey,
+                                ),
+                                CustomText(
+                                  fontSize: 20.sp,
+                                  text: signUpController.codeNo[index],
+                                  color: AppColor.white,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ), //"signUpController.traineeName[index]"
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<void> getData() async {
+    await FirebaseFirestore.instance
+        .collection('Form(Data)')
+        .doc('')
+        .snapshots()
+        .forEach((element) {
+      if (element.data()?['Trainee Name'] != null) {
+        print(element.data()?['Trainee Name']);
+        signUpController.traineeName = element.data()?['Trainee Name'];
+        signUpController.codeNo = element.data()?['Code Num'];
+      }
+      setState(() {});
+    });
+  }
+}
---
+++ lib/Features/admin/every_form.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShowOneFormAndPrint extends StatelessWidget {
+  ShowOneFormAndPrint({Key? key, required this.traineeName}) : super(key: key);
+
+  String traineeName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
